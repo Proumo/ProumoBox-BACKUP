5a07ad726912a3e73fd6c44a0dfe8f0b95535b88
alxferraz <andrelxferraz@gmail.com>
2014-01-02 19:38:28 -0200

    [André L. Ferraz]:criação da tela de criação de bbapps

--- ProumoBox/espaco_criacao.php
+++
@@ -1,5 +0,0 @@
-<?php require 'header.php'; ?>
-    <?php require 'tmp_espaco_criacao/lista_apps.php'; ?>
-    <?php require 'tmp_espaco_criacao/bbapp_frame.php'; ?>
-    <?php require 'tmp_espaco_criacao/lista_apps.php'; ?>
-<?php require 'footer.php'; ?>
---
+++ tmp_espaco_criacao/lista_apps.php
@@ -0,0 +1,25 @@
+<input  placeholder="Pesquise Apps">
+
+<div class="app well">
+    <div class="app_icon"><img class="app_icon" src="assets/img/imgs-proumo/icon3.png"></div>
+    <span class="nome">TechSpechs 1.0</span>
+    <i class="icon-plus-sign add"></i>
+    <span class="preco">R$ 0,99</span>
+    
+</div>
+
+<div class="app well">
+    <div class="app_icon"><img  class="app_icon" src="assets/img/imgs-proumo/icon2.png"></div>
+    <span class="nome">easyWS 1.0</span>
+    <i class="icon-plus-sign add"></i>
+    <span class="preco">R$ 1,99</span>
+    
+</div>
+
+<div class="app well">
+    <div class="app_icon"><img class="app_icon"  src="assets/img/imgs-proumo/icon1.jpg"></div>
+    <span class="nome">Animation3D</span>
+    <i class="icon-plus-sign add"></i>
+    <span class="preco">R$ 0,99</span>
+    
+</div>
