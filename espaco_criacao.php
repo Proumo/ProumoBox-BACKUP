e8e2d82ba3f833a02a59f646a1448304ef783b4e
alxferraz <andrelxferraz@gmail.com>
2014-01-03 13:33:36 -0200

    Revert "[André L. Ferraz]:criação da tela de criação de bbapps"
    This reverts commit 5a07ad726912a3e73fd6c44a0dfe8f0b95535b88.

--- espaco_criacao.php
+++
@@ -1,5 +0,0 @@
-<?php require 'header.php'; ?>
-    <div class="apps_container well"><?php require 'tmp_espaco_criacao/lista_apps.php'; ?></div>
-    <div class="bbapp_frame well "><?php require 'tmp_espaco_criacao/bbapp_frame.php'; ?></div>
-    <div class="carrinho_apps well"><?php require 'tmp_espaco_criacao/carrinho_apps.php'; ?></div>
-<?php require 'footer.php'; ?>
