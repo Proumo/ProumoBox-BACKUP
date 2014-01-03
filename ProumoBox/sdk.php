f2ad8897c01b09aefdaf5b4f8a09443c63ddf255
TonGarcia <ton_unb@hotmail.com>
2014-01-03 14:10:24 -0200

    [Ilton Garcia]: refatoração na estrutura de rows e containers

--- ProumoBox/sdk.php
+++ ProumoBox/sdk.php
@@ -1,12 +1,18 @@
 <?php require 'header.php'; ?>
-    <div class="container">
-        <div class="row">
-            <?php require 'tmp_sdk/sdk_header.php'; ?>
-            <?php require 'tmp_sdk/arvore_proj.php'; ?>
-            <?php require 'tmp_sdk/codigo_fonte.php'; ?>
-            <?php require 'tmp_sdk/preview.php'; ?>
-        </div>
+<div class="container">
+    <div class="row">
+        <?php require 'tmp_sdk/sdk_header.php'; ?>
+    </div>
+</div>
+
+<div id="sdk_container" class="container">
+    <div class="row">
+        <?php require 'tmp_sdk/arvore_proj.php'; ?>
+        <?php require 'tmp_sdk/codigo_fonte.php'; ?>
     </div>
 
-    <?php require 'tmp_sdk/console.php'; ?>
+    <div class="row">
+        <?php require 'tmp_sdk/console.php'; ?>
+    </div>
+</div>
 <?php require 'footer.php'; ?>
