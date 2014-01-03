3efa1c84e3d822cc9a1dccc4d1fe396a2d31702d
TonGarcia <ton_unb@hotmail.com>
2014-01-03 17:16:14 -0200

    [Ilton Garcia]: encapsulamento d eum container dentor do header

--- ProumoBox/tmp_home/h_header.php
+++ ProumoBox/tmp_home/h_header.php
@@ -1,6 +1,8 @@
-<div class="row">
-	<header class="span12">
-	    <button class="btn btn-primary">Fazer Login</button>
-            <div id="brand"><img src="assets/img/imgs-proumo/logo-256.png"> <span>ProumoBox</span></div>
-	</header>
+<div class="container">
+    <div class="row">
+            <header class="span12">
+                <button class="btn btn-primary">Fazer Login</button>
+                <div id="brand"><img src="assets/img/imgs-proumo/logo-256.png"> <span>ProumoBox</span></div>
+            </header>
+    </div>
 </div>
