afb115d77154ae8a1c6859632f229f097cb1161a
TonGarcia <ton_unb@hotmail.com>
2014-01-02 22:49:15 -0200

    [Ilton Garcia]: Criação estrutura básica da parte do SDK

---
+++ ProumoBox/sdk.php
@@ -0,0 +1,7 @@
+<?php require 'header.php'; ?>
+    <?php require 'tmp_sdk/sdk_header.php'; ?>
+    <?php require 'tmp_sdk/arvore_proj.php'; ?>
+    <?php require 'tmp_sdk/codigo_fonte.php'; ?>
+    <?php require 'tmp_sdk/preview.php'; ?>
+    <?php require 'tmp_sdk/console.php'; ?>
+<?php require 'footer.php'; ?>
