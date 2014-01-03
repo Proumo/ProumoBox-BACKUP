e9f203e795ef14986b0086402ede96888e8ac527
alxferraz <andrelxferraz@gmail.com>
2014-01-03 16:41:08 -0200

    [André L. Ferraz]:criação da tela de criação de bbapps

--- ProumoBox/espaco_criacao.php
+++ ProumoBox/espaco_criacao.php
@@ -3,10 +3,10 @@
     <div class="row">
         <div id="lista_apps" class="span3"> <div class="well"> <?php require 'tmp_espaco_criacao/lista_apps.php'; ?></div></div>
         <div id="bbapp_frame" class="span9"> <div class="well"> <?php require 'tmp_espaco_criacao/bbapp_frame.php'; ?></div></div>
-       
-        <div id="carrinho_apps" class="span3"> <div class="well"> <?php require 'tmp_espaco_criacao/carrinho_apps.php'; ?></div></div>
+    </div>
+    <div class="row">
+        <div id="meus_arquivos" class="span3"> <div class="well"> <?php require 'tmp_espaco_criacao/carrinho_apps.php'; ?></div></div>
         <div id="file_preview" class="span9"> <div class="well"> <?php require 'tmp_espaco_criacao/bbapp_frame.php'; ?></div></div>
-       
     </div>
 </div>
 <?php require 'footer.php'; ?>
