031c835477f66ccc78e87f16617dde78880f3278
TonGarcia <ton_unb@hotmail.com>
2014-01-05 01:29:32 -0200

    [Ilton Garcia]:  Adição dos botões-icones para manipulação do proj do App e também criação da função de Toggle, um teste, em uma pasta

--- ProumoBox/tmp_sdk/arvore_diretorios.php
+++ ProumoBox/tmp_sdk/arvore_diretorios.php
@@ -1,7 +1,22 @@
-<div id="arvore_diretorios" class="span4">
+<div id="arvore_diretorios" class="span4" ng-controller="ArvoreDiretorioController">
+    <header> 
+        <div  id="ops_ger_arqs_dirs"> 
+            <i class="fa fa-cloud-upload tooltiper" data-original-title="Push GitHub"></i>
+            <i class="fa fa-cloud-download tooltiper" data-original-title="Pull GitHub"></i>
+            
+            <i class="fi-folder-add"></i>
+            <span><i class="fi-folder"></i> <i class="fi-x"></i></span>
+            
+            <i class="fi-page-add"></i>
+            <i class="fi-page-delete"></i>
+        </div>
+        
+        <h5> <img src="assets/img/imgs-proumo/favicon-prb.png" /> Título do projeto</h5> 
+    </header>
     
-    <header> <h5> <img src="assets/img/imgs-proumo/favicon-prb.png" /> Título do projeto</h5> </header>
-    
-    <i class="icon-folder"></i>
-    
+    <div id="frame_diretorios">
+        <div class="folder" ng-click="toggle_pasta($event)">
+            <i class="fa fa-folder{{ folder_state }}"></i> <span class="{{ folder_name }}"> Título do projeto</span>
+        </div>
+    </div>
 </div>
