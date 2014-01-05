dababcbd66f14f654b7e7debac1ff69937dd5f39
TonGarcia <ton_unb@hotmail.com>
2014-01-05 01:26:22 -0200

    [Ilton Garcia]: Criação Controller Angular da árvore e tb da função de Toogle das pastas

---
+++ ProumoBox/assets/js/app_js/angular_app/arvore_diretorios_controller.js
@@ -0,0 +1,16 @@
+var ProumoApp = angular.module('ProumoApp', []);
+
+ProumoApp.controller('ArvoreDiretorioController', function($scope, $http) {
+    
+    // abre e fecha os diretórios (toggle) 
+    $scope.toggle_folder = function(event) {
+        if ($scope.folder_state === undefined){
+            $scope.folder_state = '-open';
+            $scope.folder_name = 'strong';
+        }else {
+            $scope.folder_name = undefined;
+            $scope.folder_state = undefined;
+        }
+    }// /toggle_folder
+
+});
