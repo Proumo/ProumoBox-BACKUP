faaa055efd23a0791bca51ece5f8f35898721e37
TonGarcia <ton_unb@hotmail.com>
2014-01-05 10:48:32 -0200

    [Ilton Garcia]: criação da tag header para que se adeque ao padrão da arvore de diretório

--- ProumoBox/tmp_sdk/arquivo_editor.php
+++ ProumoBox/tmp_sdk/arquivo_editor.php
@@ -1,6 +1,8 @@
 <div id="arquivo_editor" class="span8 frame_sdk">
-    <ul class="breadcrumb">
-        <li class="strong"> Título do Projeto <span class="divider">/</span></li>
-        <li class="active"> index.html </li>
-    </ul>
+    <header>
+        <ul class="breadcrumb">
+            <li class="strong"> Título do Projeto <span class="divider">/</span></li>
+            <li class="active"> index.html </li>
+        </ul>
+    </header>
 </div>
