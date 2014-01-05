f2df9f0703f0bdb8530b3841f3badbde5e490efb
TonGarcia <ton_unb@hotmail.com>
2014-01-05 02:11:39 -0200

    [Ilton Garcia]: refatoração da arvore para poder manipular o seu header

--- ProumoBox/tmp_sdk/arvore_diretorios.php
+++ ProumoBox/tmp_sdk/arvore_diretorios.php
@@ -1,17 +1,19 @@
-<div id="arvore_diretorios" class="span4" ng-controller="ArvoreDiretorioController">
+<div id="arvore_diretorios" class="span4 frame_sdk" ng-controller="ArvoreDiretorioController">
     <header> 
-        <div  id="ops_ger_arqs_dirs"> 
-            <i class="fa fa-cloud-upload tooltiper" data-original-title="Push GitHub"></i>
-            <i class="fa fa-cloud-download tooltiper" data-original-title="Pull GitHub"></i>
-            
-            <i class="fi-folder-add"></i>
-            <span><i class="fi-folder"></i> <i class="fi-x"></i></span>
-            
-            <i class="fi-page-add"></i>
-            <i class="fi-page-delete"></i>
+        <div>
+            <div  id="ops_ger_arqs_dirs"> 
+                <i class="fa fa-cloud-upload tooltiper" data-original-title="Push GitHub"></i>
+                <i class="fa fa-cloud-download tooltiper" data-original-title="Pull GitHub"></i>
+
+                <i class="fi-folder-add"></i>
+                <span><i class="fi-folder"></i> <i class="fi-x"></i></span>
+
+                <i class="fi-page-add"></i>
+                <i class="fi-page-delete"></i>
+            </div>
+
+            <h5> <img src="assets/img/imgs-proumo/favicon-prb.png" /> Título do projeto</h5> 
         </div>
-        
-        <h5> <img src="assets/img/imgs-proumo/favicon-prb.png" /> Título do projeto</h5> 
     </header>
     
     <div id="frame_diretorios">
