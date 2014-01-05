d6fb5ebed066e48ecbe76e4fe3a5fac9f8ba26da
TonGarcia <ton_unb@hotmail.com>
2014-01-05 11:38:44 -0200

    [Ilton Garcia]: adição das classes span a barra header para ficar responsiva e adição da parte do usuário

--- ProumoBox/tmp_home/h_header.php
+++ ProumoBox/tmp_home/h_header.php
@@ -1,8 +1,29 @@
 <div class="container">
     <div class="row">
-            <header class="span12">
-                <button class="btn btn-primary">Fazer Login</button>
-                <div id="brand"><img src="assets/img/imgs-proumo/logo-256.png"> <span>ProumoBox</span></div>
+            <header id="header_nav" class="span12">
+                <div class='row'>
+                    <div id="brand" class="span2"><img src="assets/img/imgs-proumo/logo-256.png"> <span>ProumoBox</span></div>
+                    
+                    <nav class='offset2 span6'>
+                        <ul class="nav nav-pills">
+                            <li class=""><a href="#">Produto</a></li>
+                            <li class=""><a href="#">Anunciante</a></li>
+                            <li class=""><a href="#">SDK</a></li>
+                        </ul>
+                    </nav>
+                    
+                    <div id='logar' class='span2' style='display:none;'>
+                        <button class="btn btn-primary">Fazer Login</button>
+                    </div>
+                    
+                    <div id='perfil_rapido' class='span2'>
+                        <img src='http://www.gravatar.com/avatar/b85daa4cfdfabe2071abd389aadc5aeb?s=200'>
+                        
+                        <div>
+                            
+                        </div>
+                    </div>
+                </div>
             </header>
     </div>
 </div>
