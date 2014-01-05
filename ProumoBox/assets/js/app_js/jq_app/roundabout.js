c11cd581a87dca9306b2e3623127cf127cb0f46e
TonGarcia <ton_unb@hotmail.com>
2014-01-05 01:27:47 -0200

    [Ilton Garcia]: mudança de local do roundabout

---
+++ ProumoBox/assets/js/app_js/jq_app/roundabout.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+                $('ul').roundabout({
+                    enableDrag: true,
+                    tilt: 70,
+                    minOpacity: 0.001,
+                    minZ:50
+                    
+                    
+                });
+            });
