6227c30e4c841631242d48da616a50ee17234b24
TonGarcia <ton_unb@hotmail.com>
2014-01-05 01:30:43 -0200

    [Ilton Garcia]: Refatoração na escrita do Código

--- ProumoBox/index.php
+++ ProumoBox/index.php
@@ -1,4 +1,5 @@
     <?php require 'header.php'; ?>
+    
     <div class="container">
 
         <ul>
@@ -15,10 +16,11 @@
             <iframe class="offset3 span6" height="360" src="//www.youtube.com/embed/jofNR_WkoCE" frameborder="0" allowfullscreen></iframe>
         </div>
     </div>
-    <script src="assets/js/jquery/jq-1.10.2.min.js"></script>
+
     <script src="assets/js/jquery/roundabout/jquery.roundabout.js"></script>
     <link type="text/css" rel="stylesheet" href="assets/css/roundabout/estilo_round.css">
-    <script src="assets/js/app_js/roundabout.js"></script>
+    <script src="assets/js/app_js/jq_app/roundabout.js"></script>
     <script src="assets/js/jquery/roundabout/jquery.event.drag-2.2.js"></script>
     <script src="assets/js/jquery/roundabout/jquery.event.drop-2.2.js"></script>
+    
     <?php require 'footer.php'; ?>
