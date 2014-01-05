cc5a87113ee5502dd1665a7a4a6326b0d9f93947
TonGarcia <ton_unb@hotmail.com>
2014-01-05 02:25:41 -0200

    [Ilton Garcia]: refatoração do breadcumbs para apresentar como o index.html estivesse aberto

--- ProumoBox/tmp_sdk/arquivo_editor.php
+++ ProumoBox/tmp_sdk/arquivo_editor.php
@@ -1,7 +1,6 @@
-<div>
-
-    <div id="arquivo_editor" class="span8">
-        codigo fonte
-    </div>
-    
+<div id="arquivo_editor" class="span8 frame_sdk">
+    <ul class="breadcrumb">
+        <li class="strong"> Título do Projeto <span class="divider">/</span></li>
+        <li class="active"> index.html </li>
+    </ul>
 </div>
